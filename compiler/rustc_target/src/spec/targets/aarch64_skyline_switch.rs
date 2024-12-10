f83ee95774c3f9eef345042721e48e541699b861
⭐️NINIKA⭐️ <dcnick3@users.noreply.github.com>
2024-12-10 11:55:32 +0300

    Fix skyline std compilation

--- compiler/rustc_target/src/spec/targets/aarch64_skyline_switch.rs
+++ compiler/rustc_target/src/spec/targets/aarch64_skyline_switch.rs
@@ -1,10 +1,11 @@
-use super::{Cc, LinkerFlavor, Lld, PanicStrategy, RelroLevel, Target, TargetOptions};
+use super::super::{Cc, LinkerFlavor, Lld, PanicStrategy, RelroLevel, Target, TargetOptions};
 
 const LINKER_SCRIPT: &str = include_str!("./aarch64_nintendo_switch_freestanding_linker_script.ld");
 
 /// A base target for Nintendo Switch devices using a pure LLVM toolchain.
-pub fn target() -> Target {
+pub(crate) fn target() -> Target {
     Target {
+        metadata: Default::default(),
         llvm_target: "aarch64-unknown-none".into(),
         pointer_width: 64,
         data_layout: "e-m:e-i8:8:32-i16:16:32-i64:64-i128:128-n32:64-S128-Fn32".into(),
