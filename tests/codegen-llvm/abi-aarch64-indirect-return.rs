2d3aa9b46411b06dc035d8cb061d89c4a0027cd3
⭐️NINIKA⭐️ <dcnick3@users.noreply.github.com>
2025-08-29 15:47:27 +0300

    fix test

--- tests/codegen-llvm/abi-aarch64-indirect-return.rs
+++ tests/codegen-llvm/abi-aarch64-indirect-return.rs
@@ -13,7 +13,7 @@
 extern crate minicore;
 use minicore::*;
 
-// CHECK: define dso_local void @has_indirect_return_abi(ptr sret %result_location, i64 %value)
+// CHECK: define dso_local void @has_indirect_return_abi(ptr sret([8 x i8]) %result_location, i64 %value)
 #[no_mangle]
 pub extern "aarch64-indirect-return" fn has_indirect_return_abi(
     result_location: *mut i64,
